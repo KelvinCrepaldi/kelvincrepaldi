7e1c563fe4505b1d68ad3b573b47e9b20b67c23c
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2025-08-08 17:19:48 -0300

    updates

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,13 @@
+import { motion } from "framer-motion";
 export default function Contact() {
-    return (
-      <div className="">
-          Contact
-      </div>
-    );
-  }
-  
+  return (
+    <div className="">
+      Contact
+      <motion.div className=" text-4xl"
+        animate={{ scaleX: 0, }}
+        
+      
+      > V </motion.div>
+    </div>
+  );
+}
