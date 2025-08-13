990b85046e362256f5f589aa34cbc0c7e441c50b
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2025-08-13 17:10:28 -0300

    remove blog path from nav bar

--- src/routes/pages.tsx
+++ src/routes/pages.tsx
@@ -1,7 +1,6 @@
 const pages = [
   { path: "/", label: "Home", backTo: "/"},
   { path: "/project", label: "Projects", backTo: "/" },
-  { path: "/blog", label: "Blog", backTo: "/" },
 ];
 
 export default pages;
