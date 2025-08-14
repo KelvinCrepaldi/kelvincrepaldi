6dc98e6946d3fbdfedd42187b8bcf84ec744e6e2
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2025-08-14 16:26:23 -0300

    create project page features

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -2,18 +2,18 @@ import ecosmd from "@/assets/projects/ecos/ecos.md";
 import ecoslogo from "@/assets/projects/ecos/thumb.png";
 import ecosimg from "@/assets/projects/ecos/image1.png";
 
-import adamDigitalMd from "@/assets/projects/adamDigital/adamDigital.md"
-import adamDigitalImg from "@/assets/projects/adamDigital/image.png"
-import adamDigital1 from "@/assets/projects/adamDigital/image1.png"
-import adamDigital2 from "@/assets/projects/adamDigital/image2.png"
-import adamDigital3 from "@/assets/projects/adamDigital/image3.png"
-import adamDigital4 from "@/assets/projects/adamDigital/image4.png"
-import adamDigital5 from "@/assets/projects/adamDigital/image5.png"
+import adamDigitalMd from "@/assets/projects/adamDigital/adamDigital.md";
+import adamDigitalImg from "@/assets/projects/adamDigital/image.png";
+import adamDigital1 from "@/assets/projects/adamDigital/image1.png";
+import adamDigital2 from "@/assets/projects/adamDigital/image2.png";
+import adamDigital3 from "@/assets/projects/adamDigital/image3.png";
+import adamDigital4 from "@/assets/projects/adamDigital/image4.png";
+import adamDigital5 from "@/assets/projects/adamDigital/image5.png";
 
-import piratesPunchMd from "@/assets/projects/piratespunch/piratespunch.md"
-import piratesPunch from "@/assets/projects/piratespunch/thumb.png"
+import piratesPunchMd from "@/assets/projects/piratespunch/piratespunch.md";
+import piratesPunch from "@/assets/projects/piratespunch/thumb.png";
 
-type stackTypes = "React" | "Next" | "Vite" | "Express" | "Electron";
+type stackTypes = "React" | "Next" | "Vite" | "Express" | "Electron" | "PWA";
 
 export type projectLink = {
   demo?: string;
@@ -28,7 +28,11 @@ export type projectsTypes = {
   techs: stackTypes[];
   year: string;
   role: string;
-  context: "Projeto de Trabalho" | "Freelance" | "Projeto de Estudo" | "Hackathon";
+  context:
+    | "Projeto de Trabalho"
+    | "Freelance"
+    | "Projeto de Estudo"
+    | "Hackathon";
   category: string;
   status: "Concluído" | "Em andamento" | "Manutenção";
   img: string;
@@ -37,6 +41,7 @@ export type projectsTypes = {
   file?: string;
   private: boolean;
   links?: projectLink;
+  features: string[];
 };
 
 const projects: projectsTypes[] = [
@@ -45,7 +50,7 @@ const projects: projectsTypes[] = [
     name: "Adam Robo Ecos",
     description:
       "Plataforma de testes de acuidade visual integrada a dispositivo físico",
-    techs: ["React", "Vite"],
+    techs: ["React", "Vite", "PWA"],
     year: "2024",
     role: "Front-End Developer",
     context: "Projeto de Trabalho",
@@ -56,12 +61,16 @@ const projects: projectsTypes[] = [
     path: "/f63694e0-353c-42e9-b27c-9d9c0ffaf572",
     file: ecosmd,
     private: true,
+    features: [
+      "Aplicar testes com formulários",
+      "Painel de admistrador",
+      "Gerenciar testes aplicados",
+    ],
   },
-    {
+  {
     id: "e70d5cc2-95ae-43e5-a4de-1bd8a418bff0",
     name: "Protótipo Adam Robo Digital",
-    description:
-      "Plataforma de aplicação de formulários digitais",
+    description: "Plataforma de aplicação de formulários digitais",
     techs: ["React", "Vite"],
     year: "2024",
     role: "Front-End Developer",
@@ -69,20 +78,31 @@ const projects: projectsTypes[] = [
     category: "Protótipo de Formulário",
     status: "Concluído",
     img: adamDigitalImg,
-    images: [adamDigitalImg, adamDigital1, adamDigital2, adamDigital3, adamDigital4, adamDigital5],
+    images: [
+      adamDigitalImg,
+      adamDigital1,
+      adamDigital2,
+      adamDigital3,
+      adamDigital4,
+      adamDigital5,
+    ],
     path: "/e70d5cc2-95ae-43e5-a4de-1bd8a418bff0",
     file: adamDigitalMd,
     private: true,
     links: {
       demo: "https://digital.adamroboglobal.com/",
-    }
+    },
+    features: [
+      "Aplicar testes com formulários",
+      "Gerenciar testes aplicados",
+      "Importar e exportar lista de testes aplicados",
+    ],
   },
-    
-    {
+
+  {
     id: "a0b54a2f-2dc3-48f5-bf9b-410e1d9ff596",
     name: "Pirate's Punch",
-    description:
-      "E-commerce de bebidas com tematica pirata",
+    description: "E-commerce de bebidas com tematica pirata",
     techs: ["React", "Next", "Express"],
     year: "2023",
     role: "Full-Stack Developer",
@@ -97,8 +117,14 @@ const projects: projectsTypes[] = [
     links: {
       demo: "https://piratespunchdrinks.vercel.app/",
       front: "https://github.com/KelvinCrepaldi/piratespunchdrinks",
-      api: "https://github.com/KelvinCrepaldi/piratespunchdrinks-api"
-    }
+      api: "https://github.com/KelvinCrepaldi/piratespunchdrinks-api",
+    },
+    features: [
+      "Carrinho com soma automática",
+      "Responsivo com design tematizado",
+      "Histórico de compras, cartões e endereços",
+      "Lista de produtos com filtro e páginação",
+    ],
   },
 ];
 
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,14 +1,113 @@
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import useProjects from "@/hooks/useProjects";
+import { Link } from "react-router";
 
 export default function Project() {
   const { current: project } = useProjects();
 
   if (!project) return <div>Houve um erro, projeto não encontrado</div>;
 
-  const { name } = project;
+  const {
+    name,
+    img,
+    techs,
+    description,
+    role,
+    category,
+    year,
+    features,
+    links,
+  } = project;
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      {name}
-    </div>
+    <article className="flex flex-col items-center justify-center p-2 gap-10 lg:p-10 ">
+      <div className="flex flex-col lg:flex-row flex-1 lg:items-center lg:justify-center w-full h-full gap-5 text-primary mb-20 lg:mb-0">
+        <div className="flex items-start flex-col gap-2 w-full lg:w-1/4 p-5 bg-white/20">
+          <div className="text-xl flex justify-between w-full">
+            <div>{name}</div> <div className="text-primary/70">{year}</div>
+          </div>
+          <div className="flex flex-wrap gap-2 text-primary/70">
+            {techs.map((tech) => (
+              <div className="border px-2 text-sm rounded bg-primary/70 text-white">
+                {tech}
+              </div>
+            ))}
+          </div>
+          <div className="text-start leading-5">{description}</div>
+
+          <div className="leading-4">
+            <div className="">Tecnologias:</div>
+            <div className="text-start leading-5 text-primary/70">{techs}</div>
+          </div>
+
+          <div className="leading-4">
+            <div className="text-start">Tipo:</div>
+            <div className="text-start leading-5 text-primary/70">
+              {category}
+            </div>
+          </div>
+
+          <div className="leading-4">
+            <div className="text-start">Foco:</div>
+            <div className="text-start leading-5 text-primary/70">{role}</div>
+          </div>
+
+          <div className="leading-4">
+            <div className="text-start">Funcionalidades:</div>
+            <ul className="list-disc list-inside text-start">
+              {features.map((feature) => (
+                <li className="text-primary/70">{feature}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col gap-2 w-full mt-5">
+            {links?.demo && (
+              <Button asChild>
+                <Link to={links?.demo}>Demo</Link>
+              </Button>
+            )}
+            {project.private ? (
+              <div className="text-red-800">"Repositórios github privados"</div>
+            ) : (
+              <>
+                {links?.front && (
+                  <Button asChild>
+                    <Link to={links?.front}>Front-End Github Repo</Link>
+                  </Button>
+                )}
+                {links?.api && (
+                  <Button asChild>
+                    <Link to={links?.api}>Back-End Github Repo</Link>
+                  </Button>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center flex-1 w-full">
+          <Carousel className="w-[calc(100%-100px)]">
+            <div className="border-8 border-primary bg-primary">
+              <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem key={index}>
+                    <img src={img} />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </div>
+
+            <CarouselPrevious className="bg-primary text-background" />
+            <CarouselNext className="bg-primary text-background" />
+          </Carousel>
+        </div>
+      </div>
+    </article>
   );
 }
